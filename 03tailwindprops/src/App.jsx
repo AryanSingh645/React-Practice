0fc5f061d8a2f369030bd9b497dbb146b08139b9
AryanSingh645 <singhjee30476@gmail.com>
2024-02-06 14:45:22 +0530

    Hooks, Props and TailwindCSS

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   return (
     <>
-      <h1 className=''>Tailwind css</h1>
-      
+      <h1 className='bg-green-400 mb-4'>Tailwind css</h1>
+      <Card/>
+      <Card/>
     </>
   )
 }
